2036c44e550be8254d3c059bcd3052c6dd938eb7
jctubb <jctubb@utexas.edu>
2023-04-19 23:23:03 -0500

    Deploy React app to GitHub Pages

---
+++ precache-manifest.07f4d7a2ff949a9ac999263505b274d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b1b58485a0cfc88c09db",
+    "url": "/BronzeAgeTravelBlog/static/css/main.61d3c2ba.chunk.css"
+  },
+  {
+    "revision": "b1b58485a0cfc88c09db",
+    "url": "/BronzeAgeTravelBlog/static/js/main.b1b58485.chunk.js"
+  },
+  {
+    "revision": "b5dec1235d8d1d118e4f",
+    "url": "/BronzeAgeTravelBlog/static/js/1.b5dec123.chunk.js"
+  },
+  {
+    "revision": "455bae00cb79450cd028",
+    "url": "/BronzeAgeTravelBlog/static/js/2.455bae00.chunk.js"
+  },
+  {
+    "revision": "071ee71dc88d177b8e66",
+    "url": "/BronzeAgeTravelBlog/static/js/runtime~main.071ee71d.js"
+  },
+  {
+    "revision": "72e7c47c24f378d3eacc2dc6990a6143",
+    "url": "/BronzeAgeTravelBlog/index.html"
+  }
+];
