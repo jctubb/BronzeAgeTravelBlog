11b1d934c9b181fbb41c8f0bbdb859ff0c3cd8a1
jctubb <jctubb@utexas.edu>
2023-04-20 01:06:28 -0500

    Updates

--- precache-manifest.58acdcd074e9a7ae109e5d67a799ddf1.js
+++ precache-manifest.58acdcd074e9a7ae109e5d67a799ddf1.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "b1b58485a0cfc88c09db",
+    "revision": "b49e68a4b4be7e908c9e",
     "url": "/BronzeAgeTravelBlog/static/css/main.61d3c2ba.chunk.css"
   },
   {
-    "revision": "b1b58485a0cfc88c09db",
-    "url": "/BronzeAgeTravelBlog/static/js/main.b1b58485.chunk.js"
+    "revision": "b49e68a4b4be7e908c9e",
+    "url": "/BronzeAgeTravelBlog/static/js/main.b49e68a4.chunk.js"
   },
   {
     "revision": "b5dec1235d8d1d118e4f",
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/BronzeAgeTravelBlog/static/js/runtime~main.071ee71d.js"
   },
   {
-    "revision": "72e7c47c24f378d3eacc2dc6990a6143",
+    "revision": "167fbacc26da2f26e807897a215ed0b8",
     "url": "/BronzeAgeTravelBlog/index.html"
   }
 ];
