b3f45567bbe0cf6d704a5dfd99240dec33202f7b
jctubb <jctubb@utexas.edu>
2023-04-20 10:18:04 -0500

    image fix

--- src/imageListAkrotiri.js
+++ src/imageListAkrotiri.js
@@ -1,61 +1,69 @@
 import React, { Component }  from 'react';
 import './imageList.css';
-
+import Caldera from './Caldera.jpg';
+import AkroBuildings from './AkroBuildings.jpg';
+import AkroPierDoor from './AkroPierDoor.jpg';
+import AkroPottery from './AkroPottery.jpg';
+import AkroPots2 from './AkroPots2.jpg';
+import AkroStairs from './AkroStairs.jpg';
+import AkroKitchen from './AkroKitchen.jpg';
+import AkroTripods from './AkroTripods.jpg';
+import AkroWindow from './AkroWindow.jpg';
 
 const images = [
   {
     id: 1,
     title: 'Caldera of Santorini',
     description: 'This image displays then center island of Santorini and, through the fog, more of the outer ring. The ring shape of Santorini is due to volcanic activity. Interestingly, when swimming around the central island, the water is very warm.',
-    imageSrc: './Caldera.jpg',
+    imageSrc: Caldera,
   },
   {
     id: 2,
     title: 'Akrotiri Buildings',
     description: 'A view of the ruins of Akrotiri including a large complex on the left. You can see the ',
-    imageSrc: './AkroBuildings.jpg',
+    imageSrc: AkroBuildings,
   },
   {
     id: 3,
     title: 'Pier and Door Partitions?',
     description: 'Possible Pier and Door Partitions under this collapsing beam.',
-    imageSrc: './AkroPierDoor.jpg',
+    imageSrc: AkroPierDoor,
   },
   {
     id: 4,
     title: 'Akrotiri Pottery',
     description: 'Painted pottery at the site of Akrotiri. This building may have been a storage facility or workshop (Or it is just where the archaeologists put the pots).',
-    imageSrc: './AkroPottery.jpg',
+    imageSrc: AkroPottery,
   },
   {
     id: 5,
     title: 'Akrotiri Pottery Pt. 2',
     description: 'Another view of the painted pots.',
-    imageSrc: './AkroPots2.jpg',
+    imageSrc: AkroPots2,
   },
   {
     id: 6,
     title: 'Collapsing Stairs',
     description: 'A collapsing staircase that appears to be inside a building, likely damaged by an earthquake. The flat stone flooring is an interesting look into Akrotirian interior design.',
-    imageSrc: './AkroStairs.jpg',
+    imageSrc: AkroStairs,
   },
   {
     id: 7,
     title: 'Kitchen/Workshop',
     description: 'A look inside a window to see what I believe is a kitchen or pottery workshop.',
-    imageSrc: './AkroKitchen.jpg',
+    imageSrc: AkroKitchen,
   },
   {
     id: 8,
     title: 'Tripods',
     description: 'A room containing pots, including many (often broken) three-legged pots.',
-    imageSrc: './AkroTripods.jpg',
+    imageSrc: AkroTripods,
   },
   {
     id: 9,
     title: 'Window',
     description: 'A window surrounded by wooden beams. It\'s very interesting how the wood has been preserved by the ash after all this time.',
-    imageSrc: './AkroWindow.jpg',
+    imageSrc: AkroWindow,
   },
 ];
 
--- src/imageListMycenae.js
+++ src/imageListMycenae.js
@@ -1,6 +1,21 @@
 import React, { Component }  from 'react';
 import './imageList.css';
-import LionsGate from './LionsGate.jpg'
+import LionsGate from './LionsGate.jpg';
+import LionsGateClose from './LionsGateClose.jpg';
+import GraveCircleAFar from './GraveCircleAFar.jpg';
+import GraveCircleAInside from './GraveCircleAInside.jpg';
+import TholosTombEntrance from './TholosTombEntrance.jpg';
+import TholosDog1 from './TholosDog1.jpg';
+import TholosDog2 from './TholosDog2.jpg';
+import UnroofedTholos from './UnroofedTholos.jpg';
+import Cistern from './Cistern.jpg'
+import CultCenter from './CultCenter.jpg';
+import MaskofAg from './MaskofAg.jpg';
+import MasksAndDaggers from './MasksAndDaggers.jpg';
+import BoarsTuskHelm from './BoarsTuskHelm.jpg';
+import Figurines from './Figurines.jpg';
+import SideGate from './SideGate.jpg';
+
 
 const images = [
   {
@@ -14,85 +29,85 @@ const images = [
     id: 2,
     title: 'Lions Gate Closeup',
     description: 'Closeup of the lion statues that give the Lions Gate its name.',
-    imageSrc: './LionsGateClose.jpg',
+    imageSrc: LionsGateClose,
   },
   {
     id: 3,
     title: 'Grave Circle A',
     description: 'A grave circle inside the walls of Mycenae. Many important grave goods were found here.',
-    imageSrc: './GraveCircleAFar.jpg',
+    imageSrc: GraveCircleAFar,
   },
   {
     id: 4,
     title: 'Grave Circle A Interior',
     description: 'The inside of Grave Circle A. Multiple shaft graves are found here where lavish grave goods were found.',
-    imageSrc: './GraveCircleAInside.jpg',
+    imageSrc: GraveCircleAInside,
   },
   {
     id: 5,
     title: 'Tholos Tomb',
     description: 'The entrance to the \"Tomb of Clytemnesta\" tholos tomb. The roof is still intact and the stones used are a great example of ashlar masonry. Clytemnestra probably wasn\'t buried here.',
-    imageSrc: './TholosTombEntrance.jpg',
+    imageSrc: TholosTombEntrance,
   },
   {
     id: 6,
     title: 'Napping in the tomb',
     description: 'Once a tomb for a powerful leader, this tholos tomb is now a place for dogs to get some sleep in the shade.',
-    imageSrc: './TholosDog1.jpg',
+    imageSrc: TholosDog1,
   },
   {
     id: 7,
     title: 'Another Dog in the tomb',
     description: 'Ancient tombs make a great place to cool off during hot summer days.',
-    imageSrc: './TholosDog2.jpg',
+    imageSrc: TholosDog2,
   },
   {
     id: 8,
     title: 'Unroofed Tholos Tomb',
     description: 'View inside a tholos tomb without a roof.',
-    imageSrc: './UnroofedTholos.jpg',
+    imageSrc: UnroofedTholos,
   },
   {
     id: 9,
     title: 'Cistern Tunnel',
     description: 'The tunnel down into the cistern at Mycenae. Unfortunately it was blocked off when I was here.',
-    imageSrc: './Cistern.jpg',
+    imageSrc: Cistern,
   },
   {
     id: 10,
     title: 'Cult Center Ruins',
     description: 'The ruins of the cult center at Mycenae. Large amounts of evidence for religious activity were found here.',
-    imageSrc: './CultCenter.jpg',
+    imageSrc: CultCenter,
   },
   {
     id: 12,
     title: 'Mask of Agamemnon',
     description: 'Mycenaean gold artifacts at the Athens museum including the Mask of Agamemnon. Some say it\'s fake, and I wouldn\'t be surprised. It looks very different from the other gold masks.',
-    imageSrc: './MaskofAg.jpg',
+    imageSrc: MaskofAg,
   },
   {
     id: 13,
     title: 'Gold Masks and Lion Daggers',
     description: 'More gold masks displayed alongside daggers containing lion related scenes. Found in the Athens museum.',
-    imageSrc: './MasksAndDaggers.jpg',
+    imageSrc: MasksAndDaggers,
   },
   {
     id: 14,
     title: 'Boar\'s Tusk Helmet',
     description: 'A Mycenaean warrior\'s helmet made out of boar tusks found in the Athens museum.',
-    imageSrc: './BoarsTuskHelm.jpg',
+    imageSrc: BoarsTuskHelm,
   },
   {
     id: 15,
     title: 'Figurines',
     description: 'Various Mycenaean figurines on display in the museum of Athens.',
-    imageSrc: './Figurines.jpg',
+    imageSrc: Figurines,
   },
   {
     id: 16,
     title: 'The North Gate of Mycenae.',
     description: 'The second best gate at Mycenae. Constructed using very large, blocky stones in typical Mycenaean fashion.',
-    imageSrc: './SideGate.jpg',
+    imageSrc: SideGate,
   },
 ];
 
