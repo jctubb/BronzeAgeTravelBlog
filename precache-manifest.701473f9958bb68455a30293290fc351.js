6da015abfbc0f65b824a54c161d6657dc1548d3b
jctubb <jctubb@utexas.edu>
2023-04-20 00:18:07 -0500

    Deploy React app to GitHub Pages

---
+++ precache-manifest.701473f9958bb68455a30293290fc351.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "97a18b55a5c22788ae23",
+    "url": "/BronzeAgeTravelBlog/static/css/main.61d3c2ba.chunk.css"
+  },
+  {
+    "revision": "97a18b55a5c22788ae23",
+    "url": "/BronzeAgeTravelBlog/static/js/main.97a18b55.chunk.js"
+  },
+  {
+    "revision": "b5dec1235d8d1d118e4f",
+    "url": "/BronzeAgeTravelBlog/static/js/1.b5dec123.chunk.js"
+  },
+  {
+    "revision": "455bae00cb79450cd028",
+    "url": "/BronzeAgeTravelBlog/static/js/2.455bae00.chunk.js"
+  },
+  {
+    "revision": "071ee71dc88d177b8e66",
+    "url": "/BronzeAgeTravelBlog/static/js/runtime~main.071ee71d.js"
+  },
+  {
+    "revision": "43020e4a9817f451a70f9ed2e0c2c63c",
+    "url": "/BronzeAgeTravelBlog/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.04db49af1a751563f187205fdca90e26.js"
+  "/BronzeAgeTravelBlog/precache-manifest.701473f9958bb68455a30293290fc351.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/BronzeAgeTravelBlog/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
